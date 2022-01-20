45bcbbe4ed8f95db4302efa38012773739ca085b
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-01-20 17:09:49 +0800

    update seed script

--- shared/seed/src/env.ts
+++ shared/seed/src/env.ts
@@ -8,6 +8,12 @@ if (argv.project === "Gatsby") {
     logger.error(`GATSBY_DIRECTUS_URL environment variable is missing.`);
     process.exit(1);
   }
+}
+if (argv.project === "Astro") {
+  if (!process.env.PUBLIC_DIRECTUS_URL) {
+    logger.error(`PUBLIC_DIRECTUS_URL environment variable is missing.`);
+    process.exit(1);
+  }
 } else {
   if (!process.env.DIRECTUS_URL) {
     logger.error(`DIRECTUS_URL environment variable is missing.`);
@@ -16,6 +22,16 @@ if (argv.project === "Gatsby") {
 }
 
 if (
+  argv.project === "Astro" &&
+  !process.env.PUBLIC_DIRECTUS_STATIC_TOKEN &&
+  !process.env.PUBLIC_DIRECTUS_EMAIL &&
+  !process.env.PUBLIC_DIRECTUS_PASSWORD
+) {
+  logger.error(
+    `PUBLIC_DIRECTUS_STATIC_TOKEN or PUBLIC_DIRECTUS_EMAIL + PUBLIC_DIRECTUS_PASSWORD environment variables are missing.`
+  );
+  process.exit(1);
+} else if (
   !process.env.DIRECTUS_STATIC_TOKEN &&
   !process.env.DIRECTUS_EMAIL &&
   !process.env.DIRECTUS_PASSWORD
@@ -30,8 +46,19 @@ export const env = {
   url:
     argv.project === "Gatsby"
       ? process.env.GATSBY_DIRECTUS_URL
+      : argv.project === "Astro"
+      ? process.env.PUBLIC_DIRECTUS_URL
       : process.env.DIRECTUS_URL,
-  email: process.env.DIRECTUS_EMAIL,
-  password: process.env.DIRECTUS_PASSWORD,
-  staticToken: process.env.DIRECTUS_STATIC_TOKEN,
+  email:
+    argv.project === "Astro"
+      ? process.env.PUBLIC_DIRECTUS_EMAIL
+      : process.env.DIRECTUS_EMAIL,
+  password:
+    argv.project === "Astro"
+      ? process.env.PUBLIC_DIRECTUS_PASSWORD
+      : process.env.DIRECTUS_PASSWORD,
+  staticToken:
+    argv.project === "Astro"
+      ? process.env.PUBLIC_DIRECTUS_STATIC_TOKEN
+      : process.env.DIRECTUS_STATIC_TOKEN,
 };
