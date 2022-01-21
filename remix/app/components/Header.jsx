0a7051b62e5e758c4f4aa90df0c71dc962a3237d
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-01-21 17:03:43 +0800

    add Remix example (#21)

---
+++ remix/app/components/Header.jsx
@@ -0,0 +1,17 @@
+import IconMonospace from "./icons/Monospace";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="container">
+        <a href="/">
+          <IconMonospace />
+        </a>
+        <span className="header__description">
+          An example site powered by <span className="accent">Remix</span> and{" "}
+          <span className="accent">Directus</span>.
+        </span>
+      </nav>
+    </header>
+  );
+}
