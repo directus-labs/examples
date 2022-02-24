83231fbd167d7c341e79f20425da80a5874ff1e9
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-02-24 11:19:24 +0800

    fix nextjs recommendation

--- nextjs/pages/_app.jsx
+++ nextjs/pages/_app.jsx
@@ -8,19 +8,6 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Directus Next.js Example</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
-        />
       </Head>
       <DefaultLayout>
         <Component {...pageProps} />
