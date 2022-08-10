d4b0502283c81facc3390b2db7fdc61471a5d7d2
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-08-10 21:53:55 +0800

    Update Astro to 1.0

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,5 +4,10 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+	integrations: [vue()],
+	vite: {
+		ssr: {
+			noExternal: ["modern-normalize"],
+		},
+	},
 });
