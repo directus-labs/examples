a6b5f4bf65a562a2906bf765f731418c88b0b2c1
Roger Stringer <roger@datamcfly.com>
2022-10-13 09:28:10 -0700

    eleventy

--- eleventy/src/_data/articles.js
+++ eleventy/src/_data/articles.js
@@ -1,6 +1,27 @@
-const {
-  formatRelativeTime,
-} = require("../../../shared/utils/format-relative-time.cjs");
+const UNITS = {
+  year: 24 * 60 * 60 * 1000 * 365,
+  month: (24 * 60 * 60 * 1000 * 365) / 12,
+  day: 24 * 60 * 60 * 1000,
+  hour: 60 * 60 * 1000,
+  minute: 60 * 1000,
+  second: 1000,
+};
+
+const LOCALE = "en";
+
+const rtf = new Intl.RelativeTimeFormat(LOCALE, { numeric: "auto" });
+
+function formatRelativeTime(fromDate, toDate) {
+  const elapsed = fromDate - (toDate || new Date());
+
+  // "Math.abs" accounts for both "past" & "future" scenarios
+  for (let u in UNITS) {
+    if (Math.abs(elapsed) > UNITS[u] || u === "second")
+      return rtf.format(Math.round(elapsed / UNITS[u]), u);
+  }
+
+  return fromDate.toLocaleDateString(LOCALE);
+}
 
 module.exports = async function ({ directus }) {
   let response = await directus.items("articles").readByQuery({
