4d871eda2991e222a4fefc4261bfbaebabebea1e
Roger Stringer <roger@datamcfly.com>
2022-10-27 07:15:57 -0700

    update next

--- nextjs/pages/index.jsx
+++ nextjs/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Hero from "../components/Hero";
 import Article from "../components/Article";
 import { getDirectusClient } from "../lib/directus";
